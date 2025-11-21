d68da756ae4ab34810d4b2e3f1293dff7dc933dd
Maxime Belotti <87154626+maximebelotti@users.noreply.github.com>
2025-11-21 16:57:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,34 +10,31 @@ async function run() {
     }
     
     const percentage = core.getInput('percentage');
-    var message;
-    if(percentage > Math.floor(Math.random() * 100)){
-        message = '![rickroll](https://user-images.githubusercontent.com/37572049/90699500-0cc3ec00-e2a1-11ea-8d13-989526e86b0e.gif)';
-        console.log('Gottem!!');
-        
-    }else{
-        console.log('Fine you win this time.');
-        message = core.getInput('message');
-        if(message == ''){
-            return;
-        }
+    let message;
+    if (percentage > Math.floor(Math.random() * 100)) {
+      message = '![rickroll](https://user-images.githubusercontent.com/37572049/90699500-0cc3ec00-e2a1-11ea-8d13-989526e86b0e.gif)';
+      console.log('Gottem!!');
+    } else {
+      console.log('Fine you win this time.');
+      message = core.getInput('message');
+      if (message === '') {
+        return;
+      }
     }
 
     const github_token = core.getInput('GITHUB_TOKEN');
-
     const context = github.context;
-    if (context.payload.issue == null) {
-        core.setFailed('No issue found.');
-        return;
-    }
-    const issue_number = context.payload.issue.number;
 
+    // On commente le commit du workflow (événement push)
+    const sha = context.sha;
     const octokit = new github.getOctokit(github_token);
-    const new_comment = octokit.issues.createComment({
-        ...context.repo,
-        issue_number: issue_number,
-        body: message
-      });
+
+    await octokit.repos.createCommitComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commit_sha: sha,
+      body: message
+    });
 
   } catch (error) {
     core.setFailed(error.message);
